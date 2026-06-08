79273ae3ca9482428dda4b47939f7f04adbce998
Chris <127crodriguezrufino@frhsd.com>
2026-06-08 12:11:13 -0400

    .

---
+++ code/js.js
@@ -0,0 +1,66 @@
+const cars = {
+
+bugatti:{
+name:"Bugatti Chiron",
+image:"images/bugatti.jpg",
+speed:"304 MPH",
+hp:"1500 HP",
+accel:"2.3s",
+price:"$3,000,000",
+description:"Legendary hypercar."
+},
+
+lambo:{
+name:"Lamborghini Aventador",
+image:"images/lambo.jpg",
+speed:"217 MPH",
+hp:"769 HP",
+accel:"2.8s",
+price:"$507,000",
+description:"Italian V12 supercar."
+},
+
+ferrari:{
+name:"Ferrari Concept",
+image:"images/ferrari.jpg",
+speed:"220 MPH",
+hp:"830 HP",
+accel:"2.9s",
+price:"$600,000",
+description:"Futuristic Ferrari concept."
+},
+
+pagani:{
+name:"Pagani Huayra",
+image:"images/pagani.jpg",
+speed:"238 MPH",
+hp:"791 HP",
+accel:"2.8s",
+price:"$2,600,000",
+description:"Hand-built Italian hypercar."
+},
+
+smart:{
+name:"Smart Brabus",
+image:"images/smart.jpg",
+speed:"96 MPH",
+hp:"109 HP",
+accel:"9.5s",
+price:"$25,000",
+description:"Tiny but fun city car."
+}
+
+};
+
+function showCar(car){
+
+document.getElementById("carName").innerText = cars[car].name;
+document.getElementById("carImage").src = cars[car].image;
+document.getElementById("speed").innerText = cars[car].speed;
+document.getElementById("hp").innerText = cars[car].hp;
+document.getElementById("accel").innerText = cars[car].accel;
+document.getElementById("price").innerText = cars[car].price;
+document.getElementById("description").innerText =
+cars[car].description;
+
+}
